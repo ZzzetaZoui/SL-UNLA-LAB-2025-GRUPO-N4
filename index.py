7d55999d0423c380532e299b3cdd6bde26d6c4bf
ZzzetaZoui <zoebanfi1057@gmail.com>
2025-09-02 19:35:59 -0300

    Version 1.5

--- index.py
+++ index.py
@@ -1,8 +1,33 @@
 from fastapi import FastAPI
+
 app = FastAPI()
-@app.get("/hola-mundo")
-def hola_mundo():
-    return "Hello World!"
+
+#PARA BUSCAR ESTO TENEMOS QUE PONER EL PUERTO+"/equipo" en este caso
+#ESTE ES UN EJEMPLO PARA VERIFICAR QUE SIRVE 
+@app.get("/equipo")
+def equipo():
+    return "Somos el mejor equipo del mundo"
+
+#DEFINO PRIMERO USUARIOS
+class Usuario:
+    def __init__(self, email: str, password: str):
+        self.email = email
+        self.password = password
+
+
+#ACA SE VA A GENERAR LISTA DE USUARIO
+USUARIOS=[]
+
+#TENDREMOS QUE HACER YN ERIFICADOR DE QUE EL EMAIL ESTE CORRECTO Y NO REPETIDO EN LOS DATOS
+
+#BUSCAR USUARIO POR EMAIL ES LA UNICA MANERA QUE SEA DIFERENTE
+def buscar_usuario_por_email(email: str):
+    for i in USUARIOS:
+        if i.email == email:
+            return i
+    return None
+
+#ACA SE VA A GENERAR LISTA DE USUARIO
 #APARTADO PARA REGISTER
 
 #APARTADO DE LOGIN

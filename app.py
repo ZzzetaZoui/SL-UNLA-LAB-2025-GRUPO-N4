edf2f5e5b5006148f4b22239f71cc383760d831a
ZzzetaZoui <zoebanfi1057@gmail.com>
2025-09-08 19:55:53 -0300

    SEPARACION DEL ARCHIVO PARA LA APP

---
+++ app.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, status, HTTPException
+from datetime import date, datetime
+
+
+app = FastAPI()
+
+#PARA BUSCAR ESTO TENEMOS QUE PONER EL PUERTO+"/equipo" en este caso
+#ESTE ES UN EJEMPLO PARA VERIFICAR QUE SIRVE 
+@app.get("/equipo")
+def equipo():
+    return "Somos el mejor equipo del mundo"
+
+#DEFINO PRIMERO USUARIOS
+class Usuario:
+    def __init__(self, email: str, password: str, int, telefono:int, str, nombre: str, apellido:str, dni:int, fechaNacimiento:date):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+        self.fechaNacimiento = fechaNacimiento
+        self.email = email
+        self.password = password
+
+
+#ACA SE VA A GENERAR LISTA DE USUARIO
+USUARIOS=[]
+
+#TENDREMOS QUE HACER YN ERIFICADOR DE QUE EL EMAIL ESTE CORRECTO Y NO REPETIDO EN LOS DATOS
+
+#BUSCAR USUARIO POR EMAIL ES LA UNICA MANERA QUE SEA DIFERENTE
+def buscar_usuario_por_email(email: str):
+    for i in USUARIOS:
+        if i.email == email:
+            return i
+    return None
+
+#ACA SE VA A GENERAR LISTA DE USUARIO
+#APARTADO PARA REGISTER
+
+#APARTADO DE LOGIN
+
+#APARTADO PARA CORROBORAR SI ESTAMOS HABILITADOS PARA OBTENER UN TURNO

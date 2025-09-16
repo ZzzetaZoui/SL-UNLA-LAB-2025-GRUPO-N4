612d9a756b95547dd9e90e20f7de5c29e3792b73
NicoCercio12 <cercio1998@gmail.com>
2025-09-16 00:04:08 -0300

    Organizado schemas

---
+++ schemas.py
@@ -0,0 +1,47 @@
+from datetime import date, time
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+
+class Usuario(BaseModel):
+    nombre: str
+    apellido: str
+    telefono: str
+    fecha_nacimiento: date
+    email: EmailStr
+    password: str
+
+class Login(BaseModel):
+    email: EmailStr
+    password: str
+
+class PersonaBase(BaseModel):
+    nombre: str
+    apellido: str
+    dni: int
+    email: EmailStr
+    telefono: str
+    fecha_nacimiento: date
+
+class PersonaIn(PersonaBase):
+    pass
+
+class PersonaOut(PersonaBase):
+    id: int
+    activo: bool
+
+# Turnos
+class TurnoIn(BaseModel):
+    persona_id: int
+    fecha: date
+    hora: time
+    estado: str = "confirmado"
+
+class TurnoOut(BaseModel):
+    fecha: date
+    hora: str
+    persona_id: int
+
+class Estado(BaseModel):
+    hora: str
+    disponible: bool
+

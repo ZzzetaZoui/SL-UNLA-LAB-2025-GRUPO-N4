df8ea3798067b877136182619f6617e9d0ed1c56
NicoCercio12 <cercio1998@gmail.com>
2025-09-30 13:17:08 -0300

    Implementado gestion turnos (crud)

--- crud.py
+++ crud.py
@@ -28,3 +28,33 @@ def actualizar_persona(db: Session, persona_id: int, persona_data: schemas.Perso
         db.commit()
         db.refresh(p)
     return p
+
+def crear_turno(db: Session, turno: schemas.TurnoCreate):
+    db_turno = models.Turno(**turno.dict())
+    db.add(db_turno)
+    db.commit()
+    db.refresh(db_turno)
+    return db_turno
+
+def listar_turnos(db: Session):
+    return db.query(models.Turno),all()
+
+def obtener_turno(db: Session, turno_id: int):
+    return db.query(models.turno).filter(models.Turno.id == turno_id).first()
+
+def actualizar_turno(db: Session, turno_id: int, turno_data: schemas.TurnoCreate):
+    t = obtener_turno(db, turno_id)
+    if t:
+        for key, value in turno_data.dict().items():
+            setattr(t, key, value)
+        db.commit()
+        db.refresh(t)
+    return t
+
+def eliminar_turno(db: Session, turno_id: int):
+    t = obtener_turno(db, turno_id)
+    if t:
+        db.delete(t)
+        db.commit()
+
+
